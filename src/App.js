8591fd61476108e0da5e0b931e7990cf0673f9aa
Neeraj <neerajnegi97197@gmail.com>
2020-07-18 00:19:29 +0530

    component part 1

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
-import Greet from './component/Greet.js'
-import Welcome from './component/Welcome'
+import Greet from './components/Greet.js'
+import Welcome from './components/Welcome'
+import Menu from './components/MenuComponent'
 function App() {
   return (
-    <div className="App">
+    <div >
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
         </div>
       </Navbar>
-    
+
+      <Menu/>
+      
     </div>
   );
 }
